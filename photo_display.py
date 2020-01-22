4d88c3cc44b27b9605b86ba86c154dc454c1b7a5
thierrytheg <32854618+thierrytheg@users.noreply.github.com>
2020-01-21 19:34:34 -0800

    Update photo_display.py

--- photo_display.py
+++ photo_display.py
@@ -1,9 +1,10 @@
 import lcd160cr
-import pyb
+import machine
 import time
 import random
 import os
-
+import sys
+import pyb
 
 lcd= lcd160cr.LCD160CR('X')
 lcd.erase()
@@ -11,10 +12,16 @@ lcd.erase()
 os.chdir(r'/sd/pictures')
 
 while True:
-	x=random.randint(0,(len(os.listdir(os.getcwd())))-1)
-	pic=os.listdir()[x]
-	handle=open(pic,'rb').read()
-	lcd.set_pos(0, 0)	
-	lcd.jpeg(handle)
-	pyb.delay(2000)
-	lcd.erase()
+	try:
+		x=random.randint(0,(len(os.listdir(os.getcwd())))-1)
+		pic=os.listdir()[x]
+		print(x)
+		handle=open(pic,'rb').read()
+		lcd.set_pos(0, 0)	
+		lcd.jpeg(handle)
+		pyb.delay(2000)
+		lcd.erase()
+
+	except:
+		continue
+		
